5dd1b65f8d70cedf3d5fbf58687bd7c705f33b38
Suyash Mundhe <mundhesuyash31@gmail.com>
2024-07-31 23:19:26 +0530

    Authenticate User

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+const JWT_SECRET = '$uyashWebsite'
 
 //Create a User using: PORT "/api/auth/createuser" . No login required
 router.post('/createuser', [
@@ -19,18 +22,27 @@ router.post('/createuser', [
     if (result.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    //check wheather the user with same email exist already
     try {
+        //check wheather the user with same email exist already
         let user = await User.findOne({ email: req.body.email });
         if (user) {
             return res.status(400).json({ error: 'Sorry a user with a this email already exists' })
         }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password , salt);
+        //Create new user
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email
         })
-        res.json(user);
+        const data = {
+            user :{
+                id : user.id
+            }
+        }
+        const token = jwt.sign(data,JWT_SECRET);
+        res.json({token});
     } catch (error) {
         console.error(error.message);
         res.status(500).sent('Some error occured');
@@ -38,5 +50,38 @@ router.post('/createuser', [
     // .then(user=> res.json(user)).catch(err=> {console.log(err) 
     //     res.json({error: 'Please enter a unique value for email', msg : err.message})});
 })
+
+router.post('/login', [
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+], async (req, res) => {
+    //If there are errors, return Bad request and the errors  
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: 'Please try to login with valid creadentials' });
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password)
+        if (!passwordCompare) {
+            return res.status(400).json({ error: 'Please try to login with valid creadentials' });
+        }
+        const data = {
+            user :{
+                id : user.id
+            }
+        }
+        const token = jwt.sign(data,JWT_SECRET);
+        res.json({token}); 
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).sent('Internal Server Error');
+    }
+}
+)
 
 module.exports = router;
